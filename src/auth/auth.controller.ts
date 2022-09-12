97f75f71ce36b3f3983dfe8f0519f1d063ce761e
daviramosds <davirds.dev@gmail.com>
2022-09-12 15:15:01 -0300

    login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  @HttpCode(HttpStatus.OK)
   @Post('login')
   login(@Body() loginDTO: LoginDTO) {
     return this.authService.login(loginDTO);
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { LoginDTO } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
   async login(dto: LoginDTO) {
-    return dto;
+    const { username, password } = dto;
+
+    const user = await this.prisma.user.findFirst({
+      where: {
+        username: username,
+      },
+    });
+
+    if (!user) {
+      // user doest not exist
+      throw new NotFoundException('User does not exist');
+    }
+
+    if (!user.verification.status) {
+      throw new UnauthorizedException('This user is not verified');
+    }
+
+    if (!bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Password is incorrect');
+    }
+
+    // login
+    return user;
   }
 }
