a1e5ccf45d2e7058b7f3904986256cff98f2a68b
daviramosds <davirds.dev@gmail.com>
2022-09-12 05:44:40 -0300

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://davirds:12qwaszx@cluster0.5rskjhc.mongodb.net/nest-auth',
-    ),
-    UserModule,
-  ],
+  imports: [UserModule],
   controllers: [],
   providers: [PrismaService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,38 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDTO } from './dto/create-user.dto';
-import { User, UserDocument } from './schemas/user.schema';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectModel(User.name) private userModel: Model<UserDocument>,
-    private prisma: PrismaService,
-  ) {}
+  constructor(private prisma: PrismaService) {}
 
   async create(dto: CreateUserDTO) {
     console.log(dto);
 
     const { name, lastname, username, email, password } = dto;
 
-    await this.userModel.create({
-      name,
-      lastname,
+    const user = await this.prisma.user.create({
+      data: {
+        name,
+        lastname,
 
-      username,
-      email,
-      password: 'a',
+        username,
+        email,
+        password: 'a',
 
-      profile: 'a',
-      banner: 'a',
+        profile: 'a',
+        banner: 'a',
 
-      verification: {
-        status: false,
-        token: 'a',
-        TokenExpires: new Date(),
+        verification: {
+          status: false,
+          token: 'a',
+          TokenExpires: new Date(),
+        },
       },
     });
+
+    delete user.password;
+
+    return user;
   }
 }
